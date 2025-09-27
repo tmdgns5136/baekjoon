c52e9149e5b676449a1e0c821bf11c815d895f57
tmdgns5136 <tmdgns5136@naver.com>
2025-09-27 17:40:46 +0900

    Modifying an annotation

--- Baekjoon/src/BJ2023.java
+++ Baekjoon/src/BJ2023.java
@@ -3,14 +3,13 @@
 import java.io.*;
 public class BJ2023 {
     static int N;
-    static int arr[] = new int[10000001];
     static StringBuilder sb = new StringBuilder();
     static void DFS(int value, int count){
         if(count == N){
             sb.append(value + "\n");
             return;
         }
-        for(int digit : new int[]{1, 3, 7, 9}){
+        for(int digit : new int[]{1, 3, 7, 9}){ // 짝수는 안되서 홀수만, 5는 예외(5로 나눠짐)
             int next = value * 10 + digit;
             if(prime(next)){
                 DFS(next, count+1);
@@ -30,7 +29,7 @@ public class BJ2023 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        int arr1[] = {2,3,5,7};
+        int arr1[] = {2,3,5,7}; // 한 자리일 때 소수인 것부터 시작
         for(int i = 0; i<4; i++){
             DFS(arr1[i], 1);
         }
