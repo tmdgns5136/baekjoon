2e62455dc20592a30b8c69c7007a3336f28e47fd
tmdgns5136 <tmdgns5136@naver.com>
2025-11-03 22:12:04 +0900

    Add solved new problem

--- Baekjoon/src/BJ1699.java
+++ Baekjoon/src/BJ1699.java
@@ -1,26 +1,18 @@
 // 제곱수의 합(실버2)
 // DP
+// 가능한 제곱 수만 고려(제곱 수가 있어야 항의 개수가 최소)
 import java.io.*;
-import java.util.*;
 public class BJ1699 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int dp[] = new int[N+1];
         for(int i = 1; i<=N; i++){
-            if(Math.sqrt(i) == Math.floor(Math.sqrt(i))){
-                dp[i] = 1;
-            }
-            else{
-                dp[i] = i;
+            dp[i] = i;
+            for(int j = 1; j * j < i; j++){
+                dp[i] = Math.min(dp[i], dp[i - j*j]+1);
             }
         }
-        dp[1] = 1;
-        for(int i = 1; i<=N; i++){
-            for(int j = 1; i + j<=N; j++){
-                dp[i+j] = Math.min(dp[i+j], dp[i] + dp[j]);
-            }
-        }
-        System.out.print(dp[N]);
+        System.out.println(dp[N]);
     }
 }
